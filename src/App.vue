a45348bf7321fb83986856bcb6496ce984bf0b06
Hum Sopha <65023605+SophaHum@users.noreply.github.com>
2022-05-11 09:51:02 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <img alt="Vue logo" src="./assets/photo_2021-12-27_11-17-59.jpg" />
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox! By Dev Pha" />
+  <WellCome msg="New testing" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import WellComeVue from "./components/WellCome.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+    WellCome: WellComeVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WellCome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WellComeVue",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 50px 0 0;
+  margin-left: -100px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
